f66861c49f2d82b870853f04e7a6c422f76c0ae5
Subhash Vadaparthi <2300030695cseh@gmail.com>
2026-05-29 15:44:26 +0530

    Alternating Sum

---
+++ Basic Coding/Alternating Sum.py
@@ -0,0 +1,25 @@
+# Faster solution (O(1))
+
+# Observe:
+
+# If n is even → answer = -n/2
+# If n is odd → answer = (n+1)/2
+n = int(input())
+
+if n % 2 == 0:
+    print(-n // 2)
+else:
+    print((n + 1) // 2)
+
+
+#------------------------------------------
+n = int(input())
+
+s = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        s -= i
+    else:
+        s += i
+
+print(s)
