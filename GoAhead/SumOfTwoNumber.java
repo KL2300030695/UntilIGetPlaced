cf85f34db58edb050f8dbbb4bcca32d52b6eea2f
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-06-01 23:20:38 +0530

    sumoftwonumbers in java

---
+++ GoAhead/SumOfTwoNumber.java
@@ -0,0 +1,14 @@
+package UntilIGetPlaced.GoAhead;
+import java.util.Scanner;
+public class SumOfTwoNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        System.out.println("Enter the second number:");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int c=m+n;
+        System.out.println("The sum of two number is:"+c);
+    }
+    
+}
