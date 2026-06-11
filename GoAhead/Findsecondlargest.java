bcd78ff65e934dbbe7b3213448979115c9c32c97
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-06-11 10:50:12 +0530

    Finding second largest element

---
+++ GoAhead/Findsecondlargest.java
@@ -0,0 +1,36 @@
+package UntilIGetPlaced.GoAhead;
+import java.util.Scanner;
+public class Findsecondlargest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int secondLargest = findSecondLargest(arr);
+        if (secondLargest != Integer.MIN_VALUE) {
+            System.out.println("The second largest element is: " + secondLargest);
+        } else {
+            System.out.println("There is no second largest element.");
+        }
+        scanner.close();
+    }
+
+    public static int findSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+    
+}
